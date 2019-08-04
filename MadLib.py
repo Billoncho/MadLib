10cd5f4065f5c3e2790087a314c2ac864341e0d8
Billy Gene Ridgeway <37519033+Billoncho@users.noreply.github.com>
2019-08-04 11:52:47 +0800

    Add files via upload

---
+++ MadLib.py
@@ -0,0 +1,16 @@
+# MadLib.py
+# Billy Ridgeway
+# Creates a madlib using adjectives, nouns and verbs.
+
+# Asks the user to input an adjective.
+adjective = input("Please enter an adjective: ")
+
+# Asks the user to input a noun.
+noun = input("Please enter a noun: ")
+
+# Asks the user to input a verb ending in -ed.
+verb = input("Please enter a verb ending in -ed: ")
+
+# Prints "Your Madlib and then the sentence created by the user's inputs.
+print("Your MadLib: ")
+print("The", adjective, noun, verb, "over the lazy brown dog.")
